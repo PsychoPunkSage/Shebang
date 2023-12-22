aa433dccc5c5102ccf97f0b83c463308d363321a
PsychoPunkSage <abhinav.prakash319@gmail.com>
2023-12-22 19:39:31 +0530

    feat: 5th Script is Ready...

---
+++ 05_arrays.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Array
+
+myArray=( 1 "PPS" 32.19273 "AP is here" )
+echo "index:0 = ${myArray[0]}"
+echo "All the value in Array::> ${myArray[*]}"
+echo "Testing::> ${myArray}" # Array name = 1st element of Array.....
