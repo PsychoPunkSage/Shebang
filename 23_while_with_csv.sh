5131fa649a62fb31fd4274d21eb1a9cbe5ea2d12
PsychoPunkSage <abhinav.prakash319@gmail.com>
2023-12-23 18:06:29 +0530

    feat: 22th Script is Ready...#1

--- 23_while_with_csv.sh
+++ 23_while_with_csv.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # internal field seperator (IFS)
+# Ye file ke har ek row me jaayega & har ek value ko ek var me assign kardega...
+echo "================="
 while IFS="," read id name age
 do 
 	echo "ID: $id"
@@ -8,3 +10,16 @@ do
 	echo "Age: $age"
 	echo "================="
 done < ./23_csv.csv
+
+echo "================================================================"
+echo "================="
+
+# OR
+
+cat ./23_csv.csv | awk 'NR!=1 {print}' | while IFS="," read id name age
+do 
+	echo "ID: $id"
+	echo "Name: $name"
+	echo "Age: $age"
+	echo "================="
+done
