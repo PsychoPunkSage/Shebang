5492cfa62eb2976f0630f9d0adee089e9955046f
PsychoPunkSage <abhinav.prakash319@gmail.com>
2023-12-23 10:40:21 +0530

    feat: 7th Script is Ready...

---
+++ 07_string_ops.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+myVar="Hey Bro, do you know AP i.e. PsychoPunkSage is here???"
+echo "Length of 'myVar'::> ${#myVar}"
+
+# To upper case:
+echo "To UPPER CASE ::> ${myVar^^}"
+echo "To lower case ::> ${myVar,,}"
