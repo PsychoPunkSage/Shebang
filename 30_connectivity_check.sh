f3aca2cdebe731c827286a56d8b7a4cc2e4a0918
PsychoPunkSage <abhinav.prakash319@gmail.com>
2023-12-27 12:12:13 +0530

    feat: 30th script completed....

---
+++ 30_connectivity_check.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+read -p "Which site you want to Check::>  " site
+
+ping -c 12 $site
+# sleep 5s
+
+if [[ $? -eq 0 ]]
+then
+	echo "Successfully Connected <$site>"
+else
+	echo "Unable to connect <$site>"
+fi
