ae15b99e9742d2b97dc0fc8819433382a99ca615
PsychoPunkSage <abhinav.prakash319@gmail.com>
2023-12-27 11:31:45 +0530

    feat: 28th script completed....

---
+++ 28_break.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Need to find whether the given no. is present or not....
+
+read -p "Enter a random no. ::> " num
+
+for i in 1 2 3 4 5 6 7 8 9
+do
+	if [[ $num -eq $i ]]
+	then
+		echo "$num found....."
+		break
+	fi
+	echo "Number is $i"
+done
--- 28_scripts.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-# Script no. 28_init
