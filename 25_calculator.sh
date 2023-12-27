b749f9f21f7ea883c09af4813e22a4ef41bd31f8
PsychoPunkSage <abhinav.prakash319@gmail.com>
2023-12-27 10:15:01 +0530

    feat: COmpleted basic calculator #1

--- 25_calculator.sh
+++ 25_calculator.sh
@@ -8,17 +8,17 @@
 
 read -p "Enter ist no. ::> " num1
 read -p "Enter 2nd no. ::> " num2
-echo "Welcome $(whoami)"
 
+echo "Welcom ${whomami}"
 echo "Choose:"
 echo "	a.) Add"
 echo "  b.) Sub"
 echo "  c.) Mul"
 echo "  d.) Div"
- 
+
 read -p "::> " choice
 
-case $choice in
+case $choice in:
 	a)
 		let res=$(($num1+$num2))
 		echo "$num1 + $num2 => $res"
@@ -28,19 +28,14 @@ case $choice in
 		echo "$num1 - $num2 => $res"
 		;;
 	c)
-        let res=$(($num1*$num2))
-        echo "$num1 X $num2 => $res"
-        ;;
+                let res=$(($num1*$num2))
+                echo "$num1 X $num2 => $res"
+                ;;
 	d)
-		if [ -z "$num1" ] || [ -z "$num2" ]; then
-    		echo "Usage: $0 num1 num2"
-    		exit 1
-		fi
-
-        res=$(echo "scale=2; $num1 / $num2" | bc)  
-        echo "$num1 / $num2 => $res"
-        ;;
-	*)  echo "Enter valid value"
+                let res=$(($num1/$num2))
+                echo "$num1 / $num2 => $res"
+                ;;
+	*)echo "Enter valid value"
 esac
 
 		
