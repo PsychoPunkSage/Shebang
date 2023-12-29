72979821ff59e8817e0f55502a0005961e3f88ca
PsychoPunkSage <abhinav.prakash319@gmail.com>
2023-12-29 09:54:25 +0530

    feat: Zipper is ready

---
+++ Projects/zipper.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Vars
+BASE=$1
+DAYS=120
+DEPTH=1 # For "find"
+RUN=0
+
+# Check whether directory is present or not
+if [[ ! -d $BASE ]]
+then
+	echo "Directory DNE: <$BASE>"
+	exit 1
+fi
+
+# Create "archive" frolder if it DNE
+if [[ ! -d $BASE/archive ]]
+then
+	echo "Making 'Archive' folder"
+	mkdir $BASE/archive
+fi
+
+# Find the list of Files > 200MB
+for i in `find $BASE -maxdepth $DEPTH -type f -size +200MB`
+do
+	if [[ $RUN -eq 0 ]]
+	then
+		echo "[$(date "+%Y-%m-%d %H-%M-%S")] archiving $i ===> $BASE/archive"
+		gzip $i || exit 1
+		echo "Moving $i.gz ===> $BASE/archive"
+		mv $i.gz $BASE/archive || exit 1
+		echo "Successful"
+	fi
+done
